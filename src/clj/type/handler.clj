e4ef4465eb4d86343807fddcc7c33b68bcfb73a5
Théophile Villard <theophile.villard@gmail.com>
2016-05-26 22:07:48 +0100

    handler: add meta description

--- src/clj/type/handler.clj
+++ src/clj/type/handler.clj
@@ -18,6 +18,8 @@
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport"
             :content "width=device-width, initial-scale=1"}]
+    [:meta {:name "description"
+            :content "Mini game to learn to type fast."}]
     [:link {:rel "icon"
             :href "/favicon.ico"}]
     [:link {:href "https://fonts.googleapis.com/css?family=Cousine"
